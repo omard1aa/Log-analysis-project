f54f72dd286613d1fdcf9bebee8fac86eebe9ec2
odian <omardiaa27@gmail.com>
2019-03-18 21:09:50 +0000

    Log analysis project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Udacity-fsnd-log-analysis-frist-project
+Udacity Full Stack NanoDegree Log Analysis Project
+
+### Project Description
+>This is the reporting tool project that asks you questions:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1 % of requests lead to errors?
+and prints out reports (in plain text) based on the data in the database. This reporting tool is a Python3 program connecting PostgreSQL databse using the psycopg2 module.
+  
+#### Setting up and running the database:
+
+  1. usee 'vagrant up' and 'vagrant ssh' to up and run vagrant 
+
+  2. Load the data in local database using the command:
+  
+  ```
+    psql -d news -f newsdata.sql
+  ```
+  3. Use `psql` to connect to vagrant postgreSQL.
+  
+  4. Use `\c news` to connect to news Database.
+  ```
+#### Running the queries:
+  Run Reporting Tool.py using this command inside your vagrant box:
+  ```
+    $ python3 /vagrant/Reporting\ Tool.py 
+  ```
---
+++ reporting_tool.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+DBNAME = 'news'
+
+
+def getQuery(sql_query):
+    connection = psycopg2.connect(database=DBNAME, user='vagrant')
+    cursor = connection.cursor()
+    cursor.execute(sql_query)
+    results = cursor.fetchall()
+    connection.close()
+    return results
+
+
+def manipulateQueryResult(query_result):
+    query_result_output = ""
+    for line in query_result:
+        query_result_output += '\"' + str(line[0]) + "\""
+        query_result_output += ' -- '
+        query_result_output += str(line[1])
+        query_result_output += ' views.\n'
+    return query_result_output
+
+
+def manipulateQuery3_Result(query_result):
+    query_result_output = ""
+    for line in query_result:
+        query_result_output += '\"' + str(line[0]) + "\""
+        query_result_output += ' -- '
+        query_result_output += str(line[1])
+        query_result_output += '% errors.\n'
+    return query_result_output
+
+
+sql_query_1 = ('''SELECT title, COUNT(*) as views
+             FROM articles
+             JOIN log ON log.path = CONCAT('/article/', articles.slug)
+             GROUP BY title ORDER BY views DESC LIMIT 3;''')
+
+
+sql_query_2 = ('''SELECT authors.name , COUNT(*) as views
+            FROM authors JOIN articles ON articles.author = authors.id
+            JOIN log ON log.path = CONCAT('/article/', articles.slug)
+            GROUP BY authors.name ORDER BY views DESC;''')
+
+
+sql_query_3 = ('''SELECT * from (SELECT date(time), ROUND(100.0 * SUM(case log.status
+            when '200 OK' then 0 else 1 end ) / COUNT(log.status),3) as error
+            FROM log GROUP BY date(time)
+            ORDER BY error desc) as "sub query" WHERE error > 1;''')
+
+
+if(__name__ == '__main__'):
+    question = input(''' "Please choose your question to be answered:\n
+        1. What are the most popular three articles of all time?\n
+        2. Who are the most popular article authors of all time?\n
+        3. On which days did more than 1 % of requests lead to errors?\n''')
+
+    input1 = 1
+    input2 = 2
+    input3 = 3
+
+    query_result_1 = getQuery(sql_query_1)
+    result_1 = manipulateQueryResult(query_result_1)
+
+    query_result_2 = getQuery(sql_query_2)
+    result_2 = manipulateQueryResult(query_result_2)
+
+    query_result_3 = getQuery(sql_query_3)
+    result_3 = manipulateQuery3_Result(query_result_3)
+
+    if(question == str(input1)):
+        print(result_1)
+    elif(question == str(input2)):
+        print(result_2)
+    elif(question == str(input3)):
+        print(result_3)
+    else:
+        print("Not a valid option, Please enter 1, 2 or 3")
